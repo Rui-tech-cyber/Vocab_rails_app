1d5f87b0b1a31fa9873043e37f194400496c4a8d
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-09-02 17:00:46 +0900

    WordBookモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_02_074335) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_02_075957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,4 +26,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_02_074335) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "word_books", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_word_books_on_user_id"
+  end
+
+  add_foreign_key "word_books", "users"
 end
---
+++ test/models/word_book_test.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: word_books
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_word_books_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+require "test_helper"
+
+class WordBookTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
