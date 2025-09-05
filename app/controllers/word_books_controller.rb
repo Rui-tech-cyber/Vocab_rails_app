b552d3ea05fece087167377a63a515e6e723a651
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-09-05 20:32:19 +0900

    ユーザーの単語帳一覧ページを作成

--- app/controllers/word_books_controller.rb
+++ app/controllers/word_books_controller.rb
@@ -2,20 +2,20 @@ class WordBooksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @wordbooks = current_user.wordbook.order(created_at: :desc)
+    @wordbooks = current_user.word_books.order(created_at: :desc)
   end
 
   def show
-    @wordbook = current_user.wordbooks.find(params[:id])
-    @words = @wordbook.word.order(created_at: :desc)
+    @wordbook = current_user.word_books.find(params[:id])
+    @words = @wordbook.words.order(created_at: :desc)
   end
 
   def new
-    @wordbook = current_user.wordbooks.new
+    @wordbook = current_user.word_books.new
   end
 
   def create
-    @wordbook = current_user.wordbooks.new(wordbook_params)
+    @wordbook = current_user.word_books.new(wordbook_params)
     if @wordbook.save
       redirect_to wordbooks_path, notice: "単語帳を作成しました。"
     else
