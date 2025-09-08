4f6740ef7b4769b4db12dd66ca04a75f272a5467
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-09-09 02:55:03 +0900

    Examsコントローラを作成

---
+++ app/controllers/exams_controller.rb
@@ -0,0 +1,7 @@
+class ExamsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "exams/new"
+  get "exams/show"
   devise_for :users
 
   root "home#index"
