5460a44a792c6936e503c0313974b06e8a6b1313
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-09-09 03:05:34 +0900

    コントローラにExam開始処理を追加

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,7 +1,23 @@
 class ExamsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @word_book = WordBook.find(params[:word_book_id])
+    @exam = @word_book.exams.create!(user: current_user)
+
+    @word_book.words.each do |word|
+      @exam.exam_questions.create!(
+        word: word,
+        question_text: "[#{word.meaning}]に対応する英単語は？"
+      )
+    end
+
+    redirect_to exam_path(@exam)
   end
 
+
   def show
+    @exam = Exam.find(params[:id])
+    @questions = @exam.exam_questions.includes(:word)
   end
 end
