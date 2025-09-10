2c134aafd2da2e45914b7ce505cda42c4d368479
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-09-11 00:46:45 +0900

    正誤判定を修正

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -5,11 +5,13 @@ class ExamsController < ApplicationController
     @word_book = WordBook.find(params[:word_book_id])
     @exam = @word_book.exam || Exam.create!(user: current_user, word_book: @word_book)
 
+    @exam.exam_answers.destroy_all
+
     if @exam.exam_questions.empty?
       @word_book.words.each do |word|
         @exam.exam_questions.create!(
           word: word,
-          question_text: "[#{word.meaning}]に対応する英単語は？"
+          question_text: "[#{word.term}]の意味は？"
         )
       end
     end
