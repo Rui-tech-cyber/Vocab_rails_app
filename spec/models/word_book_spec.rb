5b158a0d0b169e207926cc07c1fdc35ca3b2730a
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-09-12 08:42:12 +0900

    モデルスペック完了

--- spec/models/word_book_spec.rb
+++ spec/models/word_book_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe WordBook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(name: "テスト太郎", email: "test@example.com", password: "password") }
+
+  it "タイトルがあれば有効" do
+    word_book = WordBook.new(title: "My Book", user: user)
+    expect(word_book).to be_valid
+  end
+
+  it "タイトルがなければ無効" do
+    word_book = WordBook.new(title: nil, user: user)
+    expect(word_book).not_to be_valid
+  end
 end
