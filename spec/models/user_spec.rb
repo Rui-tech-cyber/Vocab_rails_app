5b2ec02bcfe572e52c81540aa44d426da05d4d10
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-09-12 08:34:25 +0900

    Userモデルテスト完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前・メール・パスワードがあれば有効" do
+    user = User.new(name: "テスト太郎", email: "test@example.com", password: "password")
+    expect(user).to be_valid
+  end
+
+  it "名前がなければ無効" do
+    user = User.new(email: "test@example.com", password: "password")
+    expect(user).not_to be_valid
+  end
+
+  it "メールがなければ無効" do
+    user = User.new(name: "テスト太郎", password: "password")
+    expect(user).not_to be_valid
+  end
 end
